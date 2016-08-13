4e8492655c51bbec0677ff686c4d588ef413772c
Benedikt Rötsch <info@benedikt-roetsch.de>
2016-08-13 04:22:23 +0200

    dynamic folder icon

--- src/components/Sidebar/Folder/Folder.js
+++ src/components/Sidebar/Folder/Folder.js
@@ -2,18 +2,21 @@ import React from 'react'
 import classes from './Folder.scss'
 
 import folderEmpty from './assets/folder-empty.svg'
-// import folderFull from './assets/folder-full.svg'
+import folderFull from './assets/folder-full.svg'
 
-export const Folder = (props) => (
-  <div className={classes.wrapper}>
-    <div className={classes.icon}>
-      <img src={folderEmpty} />
+export const Folder = (props) => {
+  const icon = <img src={props.file.children ? folderFull : folderEmpty} />
+  return (
+    <div className={classes.wrapper}>
+      <div className={classes.icon}>
+        {icon}
+      </div>
+      <div className={classes.title}>
+        {props.file.name}
+      </div>
     </div>
-    <div className={classes.title}>
-      {props.file.name}
-    </div>
-  </div>
-)
+  )
+}
 
 Folder.propTypes = {
   file: React.PropTypes.object.isRequired
