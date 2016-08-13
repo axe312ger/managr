e8e4fff2d0384236e2c6b5a11490a4f61c6d24f7
Benedikt Rötsch <info@benedikt-roetsch.de>
2016-08-13 03:23:18 +0200

    changes socket.io port

--- src/store/createStore.js
+++ src/store/createStore.js
@@ -6,7 +6,7 @@ import makeRootReducer from './reducers'
 import createSocketIoMiddleware from 'redux-socket.io'
 import io from 'socket.io-client'
 
-const socket = io('http://localhost:3001')
+const socket = io('http://localhost:3000')
 const socketIoMiddleware = createSocketIoMiddleware(socket, 'server/')
 
 export default (initialState = {}, history) => {
