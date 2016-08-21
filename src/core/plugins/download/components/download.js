dd47509a67b296e59bbad79225147a32f9cc6e50
Benedikt Rötsch <info@benedikt-roetsch.de>
2016-08-21 03:31:27 +0200

    warn user that downloads are pretty fake at the moment

--- src/core/plugins/download/components/download.js
+++ src/core/plugins/download/components/download.js
@@ -5,8 +5,17 @@ export default React.createClass({
     return <button onClick={this.read}>Download</button>
   },
   read () {
+    if (this.props.file.stats.size > 1.5 * 1024 * 1024) {
+      window.alert(
+        [
+          'This seems to be a pretty big file.',
+          'Downloads are currently very limited to your browser.',
+          'Lets see what happens!'
+        ].join('\n')
+      )
+    }
     this.context.managr.fileAPI.readAsDataURL(this.props.file)
-      .then((dataURL, foo, bar) => {
+      .then((dataURL) => {
         const link = document.createElement('a')
         link.download = this.props.file.name
         link.href = dataURL
