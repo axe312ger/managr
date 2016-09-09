3792ccf5dc875872775c8674aae8d9313c388b00
Benedikt Rötsch <info@benedikt-roetsch.de>
2016-09-09 19:09:48 +0200

    add click event to folders in table view

--- src/routes/Files/containers/FilesTableContainer.js
+++ src/routes/Files/containers/FilesTableContainer.js
@@ -1,22 +1,36 @@
 import { connect } from 'react-redux'
 
-import { tableNode } from 'redux/selectors/display'
+import { tableNode, tableFoldersShown as tableFoldersShownSelector } from 'redux/selectors/display'
 import { path as pathSelector } from 'redux/selectors/tree'
+import { pushDir, popDir } from 'redux/modules/Files'
+import { isNode } from 'utils/datastructure'
+
 import FilesTable from '../components/FilesTable'
 
 const mapStateToProps = (state) => {
+  const tableFoldersShown = tableFoldersShownSelector(state) || false
   const node = tableNode(state) || {}
   const path = pathSelector(state)
 
   return {
     node,
     path,
-    lastUpdated: node.lastUpdated || 0
+    lastUpdated: node.lastUpdated || 0,
+    tableFoldersShown
   }
 }
 
 const mapDispatchToProps = (dispatch) => {
-  return {}
+  return {
+    pushDir: (dir) => {
+      if (isNode(dir)) {
+        dispatch(pushDir(dir))
+      }
+    },
+    popDir: () => {
+      dispatch(popDir())
+    }
+  }
 }
 
 export default connect(mapStateToProps, mapDispatchToProps)(FilesTable)
